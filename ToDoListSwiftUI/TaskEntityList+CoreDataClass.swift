b0d269af2173e347876bf1337e6efea202af48c8
IvanPrasetyoNugroho <ivanprasetyo559@gmail.com>
2023-07-25 10:21:31 +0700

    Initialized Core Data

---
+++ ToDoListSwiftUI/TaskEntityList+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  TaskEntityList+CoreDataClass.swift
+//  
+//
+//  Created by MacBook Noob on 25/07/23.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(TaskEntityList)
+public class TaskEntityList: NSManagedObject {
+
+}
---
+++ ToDoListSwiftUI/TaskEntityList+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  TaskEntityList+CoreDataProperties.swift
+//  
+//
+//  Created by MacBook Noob on 25/07/23.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension TaskEntityList {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TaskEntityList> {
+        return NSFetchRequest<TaskEntityList>(entityName: "TaskEntityList")
+    }
+
+    @NSManaged public var desc: String?
+    @NSManaged public var dueDate: Date?
+    @NSManaged public var title: String?
+    @NSManaged public var id: UUID?
+
+}
+
+extension TaskEntityList : Identifiable {
+
+}
